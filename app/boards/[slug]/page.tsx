37af0d8019c702982b6643ed8aa8b0d8e2ae8f34
myjyby <myjyby@gmail.com>
2024-11-13 21:46:29 +0100

    added location to board cartouche and improved info image

--- app/boards/[slug]/page.tsx
+++ app/boards/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import Hero from './Hero';
-import Info from './Info';
 import Content from './Content';
 import Navbar from '@/app/ui/components/Navbar';
 import Footer from '@/app/ui/components/Footer';
@@ -41,8 +40,7 @@ export default async function Page({ params, searchParams }: incomingRequestPara
     <>
       <Navbar />
       <Hero title={title} counts={counts} total={total} contributors={contributors} creatorName={creatorName} platforms={platforms} searchParams={sParams} />
-      <Info description={description} />
-      <Content searchParams={sParams} platforms={platforms} pads={data.pads} tabs={tabs} pages={pages} board={+slug} />
+      <Content searchParams={sParams} platforms={platforms} pads={data.pads} tabs={tabs} pages={pages} board={+slug} description={description} />
       <Footer />
     </>
   );
