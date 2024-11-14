8e838fa3d4893cb8a54b80dea8b420d74f5b6101
myjyby <myjyby@gmail.com>
2024-11-14 16:45:36 +0100

    created pad structure

---
+++ app/ui/pad/index.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/app/ui/components/Navbar';
+import Hero from './Hero';
+import Content from './Content';
+import Footer from '@/app/ui/components/Footer';
+
+export default function Pad() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Content />
+      <Footer />
+    </>
+  );
+}
