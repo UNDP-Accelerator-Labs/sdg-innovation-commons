3e09cd338ceb4a39212dd31328d819b73cb5ca52
Adedapo Aderemi <adedapopaul@yahoo.com>
2024-11-20 16:02:50 +0000

    remove innovation commons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '@/app/ui/global.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'SDG Innovation Commons',
+  title: 'SDG Commons',
   description: 'Building a 21st-century architecture for global public goods, requires sharing openly and scaling data, insights, solutions and next practices for the Sustainable Development Goals (SDGs). Join the Accelerator Labs on this journey as we open up our body of work, and come shape the SDG Commons with us.',
 }
 
