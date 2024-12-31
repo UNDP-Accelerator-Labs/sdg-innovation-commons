382eeeb314b0437f3e8f11008d512408b3caecb2
Alberto Cottica <alberto@cottica.net>
2024-12-31 16:48:32 +0100

    Created prototype metaboard on tourism
    Actual boards still missing

---
+++ app/lib/data/collection/tempData/sustainable tourism.ts
@@ -0,0 +1,59 @@
+export const collection = {
+	id: 'sustainable-tourism',
+	title: 'Sustainable Tourism',
+	description: 'Modern tourism is a major driver of economic progress. It accounts for 10% of global GDP and employment, 7% of global exports and encompasses a growing number ‎of new destinations. At the same time, if not well-managed, it can bring environmental pressure and deepen societal harms such as inequalities.',
+	creatorName: 'Alberto Cottica',
+	mainImage: '/images/bg-st.jpg',
+	sections: [
+		{
+			title: 'UNDP and Tourism',
+			items: [
+				{ 
+					type: 'txt', 
+					txt: 'A dedicated agency, UN Tourism (formerly UNWTO), carries the primary mandate to promote SDGs-aligned tourism within the United Nations. However, UNDP’s own mandates on inclusive growth and a healthy environment often lead it to dealing with tourism. For example, the SDG Value Chain global program supports micro, small and medium enterprises to enhance their competitiveness and aggregate into sustainable value chains. Tourism is one of the sectors it focuses in, notably in the Dominican Republic.' 
+				},
+				{ 
+					type: 'txt', 
+					txt: 'UN Tourism and UNDP co-authored <a href="https://www.e-unwto.org/doi/book/10.18111/9789284419401">a major report</a> in 2019, where they outlined a roadmap for tourism and the SDGs leading to 2030. The report singles out three main axes for intervention: a fit-for-purpose governance of tourism; putting the SDGs at the core of the business model of private sector companies; and new approaches to financing tourism.'
+				},
+				{
+					{ 
+						type: 'txt', 
+						txt: 'In mid-2024, UNDP’s Strategic Innovation Unit was supporting tourism-related portfolios in seven country offices: Armenia (Cultural Tourism/Sustainable Tourism), Azerbaijan (Smart Old City), Dominican Republic (Transforming tourism sector) Egypt, (Rethinking Tourism), Indonesia (Blue Eco-tourism and Food Systems/Sustainable Tourism and Food Systems), Maldives (Future of Work: Towards women & youth empowerment, sustainable tourism and blue economy – finished in 2023), Thailand (Rethinking Sustainable Tourism).'
+					},
+					{ 
+						type: 'txt', 
+						txt: 'In 2024, the Global Environment Facility has asked UNDP to take the lead on the development of iCOAST (Integrated Collaborative Approaches for Sustainable Tourism), which is likely to involve, among others, UNEP, UNIDO, WWF and multilateral development banks. Its goal is to support and incentivize governments, private sector and local communities to introduce sustainable tourism practices, anchored in local sustainable supply chains linked to the tourism sector. To know more about this project, contact the <a href = "https://www.undp.org/chemicals-waste">Chemicals and Waste Management Hub</a>.'
+					},
+				},
+			]
+		},
+		{
+			title: 'Main directions for R&D on sustainable tourism',
+			items: [
+				{ 
+					type: 'txt', 
+					txt: 'In 2024, sustainable tourism had emerged as an important focus of UNDP\'s R&D work, engaging over UNDP Accelerator Labs from all regions. Their exploration follows four main directions.' 
+				},
+				{
+					type: 'list',
+					items: [
+						'<b>Developing offers to serve new market niches</b>. Evaluating emerging niches in the tourism market, with a view to spreading the benefits of tourism to more locations and reducing seasonality. Example: Jordan\'s <a href="https://www.undp.org/jordan/blog/madaba-travellers-personas">portfolio for the city of Madaba</a>.',
+						'<b>Community-based tourism</b>. Connecting and structuring local businesses so that they can compete with global tour operators and keep a larger share of the value in the destination country. Example: Thailand\'s <a href = "https://www.undp.org/thailand/blog/ahead-of-curve-inclusive-tourism">community-based inclusive tourism approach</a>.',
+						'<b>Tourism and environmental sustainability</b>. Improving the health of the environment in order to support the attractiveness of tourists destination, and seeking to align the tourism sector\'s business models with environmental sustainability. Example: Lesotho\'s <a href = "https://www.undp.org/lesotho/blog/hunt-ray-finned-smallmouth">program to incentivize herd-boys to stewardship of the natural environment</a>.',
+						'<b>Digital data to understand the tourists\’ experience</b>. Using data scraped from social media and AI-powered analysis to better understand the experience of tourists. Example: Tanzania\'s <a hfref = "https://www.undp.org/tanzania/blog/re-thinking-tourism-zanzibar-how-artificial-intelligence-creating-value-industry">sentiment analysis of tourism in Zanzibar.</a>' ,
+					]
+				},
+			]
+		},
+	],
+	highlights: [],
+	boards: [
+		440,
+		439,
+		438,
+		437,
+		436,
+		435,
+	],
+}
