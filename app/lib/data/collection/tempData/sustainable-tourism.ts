bc110e31e00f813d288e3f1b405b0c3268c70813
Adedapo Aderemi <adedapopaul@yahoo.com>
2025-01-02 15:18:53 +0000

    update

--- app/lib/data/collection/tempData/sustainable-tourism.ts
+++ app/lib/data/collection/tempData/sustainable-tourism.ts
@@ -3,7 +3,7 @@ export const collection = {
 	title: 'Sustainable Tourism',
 	description: 'Modern tourism is a major driver of economic progress. It accounts for 10% of global GDP and employment, 7% of global exports and encompasses a growing number ‎of new destinations. At the same time, if not well-managed, it can bring environmental pressure and deepen societal harms such as inequalities.',
 	creatorName: 'Alberto Cottica',
-	mainImage: '/images/bg-st.jpg',
+	mainImage: '/images/bg-st.png',	
 	sections: [
 		{
 			title: 'UNDP and Tourism',
@@ -16,15 +16,13 @@ export const collection = {
 					type: 'txt', 
 					txt: 'UN Tourism and UNDP co-authored <a href="https://www.e-unwto.org/doi/book/10.18111/9789284419401">a major report</a> in 2019, where they outlined a roadmap for tourism and the SDGs leading to 2030. The report singles out three main axes for intervention: a fit-for-purpose governance of tourism; putting the SDGs at the core of the business model of private sector companies; and new approaches to financing tourism.'
 				},
-				{
-					{ 
-						type: 'txt', 
-						txt: 'In mid-2024, UNDP’s Strategic Innovation Unit was supporting tourism-related portfolios in seven country offices: Armenia (Cultural Tourism/Sustainable Tourism), Azerbaijan (Smart Old City), Dominican Republic (Transforming tourism sector) Egypt, (Rethinking Tourism), Indonesia (Blue Eco-tourism and Food Systems/Sustainable Tourism and Food Systems), Maldives (Future of Work: Towards women & youth empowerment, sustainable tourism and blue economy – finished in 2023), Thailand (Rethinking Sustainable Tourism).'
-					},
-					{ 
-						type: 'txt', 
-						txt: 'In 2024, the Global Environment Facility has asked UNDP to take the lead on the development of iCOAST (Integrated Collaborative Approaches for Sustainable Tourism), which is likely to involve, among others, UNEP, UNIDO, WWF and multilateral development banks. Its goal is to support and incentivize governments, private sector and local communities to introduce sustainable tourism practices, anchored in local sustainable supply chains linked to the tourism sector. To know more about this project, contact the <a href = "https://www.undp.org/chemicals-waste">Chemicals and Waste Management Hub</a>.'
-					},
+				{ 
+					type: 'txt', 
+					txt: 'In mid-2024, UNDP’s Strategic Innovation Unit was supporting tourism-related portfolios in seven country offices: Armenia (Cultural Tourism/Sustainable Tourism), Azerbaijan (Smart Old City), Dominican Republic (Transforming tourism sector) Egypt, (Rethinking Tourism), Indonesia (Blue Eco-tourism and Food Systems/Sustainable Tourism and Food Systems), Maldives (Future of Work: Towards women & youth empowerment, sustainable tourism and blue economy – finished in 2023), Thailand (Rethinking Sustainable Tourism).'
+				},
+				{ 
+					type: 'txt', 
+					txt: 'In 2024, the Global Environment Facility has asked UNDP to take the lead on the development of iCOAST (Integrated Collaborative Approaches for Sustainable Tourism), which is likely to involve, among others, UNEP, UNIDO, WWF and multilateral development banks. Its goal is to support and incentivize governments, private sector and local communities to introduce sustainable tourism practices, anchored in local sustainable supply chains linked to the tourism sector. To know more about this project, contact the <a href = "https://www.undp.org/chemicals-waste">Chemicals and Waste Management Hub</a>.'
 				},
 			]
 		},
