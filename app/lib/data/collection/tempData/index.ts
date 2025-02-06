1c21cf1465bd891fe4eeab152a3b771da74ef4c9
Alberto Cottica <alberto@cottica.net>
2025-02-06 18:01:58 +0100

    Added a metaboard on African Agritech solutions

--- app/lib/data/collection/tempData/index.ts
+++ app/lib/data/collection/tempData/index.ts
@@ -2,10 +2,12 @@ import { collection as circularEcnomy } from './circular-economy';
 import { collection as foodSystems } from './food-systems';
 import { collection as digitalFinancialInclusion } from './digital-financial-inclusion';
 import { collection as sustainableTourism } from './sustainable-tourism';
+import { collection as africanAgritech } from './african-solutions-agritech';
 
 export const collection = [
 	circularEcnomy,
 	foodSystems,
 	digitalFinancialInclusion,
 	sustainableTourism,
+	africanAgritech,
 ]
