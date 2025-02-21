34f37e03062065c6d541365800461ae3eb5064f3
Alberto Cottica <alberto@cottica.net>
2025-02-21 15:48:09 +0100

    Added solutions and descriptions to the greentech metaboard

--- app/lib/data/collection/tempData/african-solutions-greentech.ts
+++ app/lib/data/collection/tempData/african-solutions-greentech.ts
@@ -9,15 +9,16 @@ export const collection = {
 			items: [
 			{ 
 				type: 'txt', 
-				txt: 'Research shows that most innovation is not made by companies or university, but by ordinary people trying to improve their lives, help their loved ones, or simply have fun. A <a target="_blank" class="text-blue-500 cursor-pointer"  href="https://www.undp.org/sites/g/files/zskgke326/files/2023-07/undp_informal_innovation_in_south_africa_final.pdf" > recent study </a> estimates one million grassroots innovators – 2.5% of the population – in South Africa alone. Scanning SDG Commons data can give a sense of what grassroots innovators are working on when it comes to inventing, developing, or adopting "green" technology that helps preserve the environment and a stable climate. We started by selecting the solutions mapped on the 58 African countries (about 3,000); next, we used a Large Language Model to select those that aligned with the definition of "green/enercgy/climate tech" used by the Timbuktoo initiative. Finally, we went through about 120 of the solutions most closely aligned with that definition, and created several boards, each one gathering solutions that deployed a specific technological strategy, like "use sensors and actuators to reduce the consumption of scarce agricultural inputs, such as water" or "connect agricultural machines to solar panels to allow for a degree of mechanization even in rural areas where access to energy is problematic." More on the methodology can be found <a target="_blank" class="text-blue-500 cursor-pointer" href=https://github.com/UNDP-Accelerator-Labs/timbuktoo-hub-solutions/blob/main/README.md">here</a>.' 
+				txt: 'Research shows that most innovation is not made by companies or university, but by ordinary people trying to improve their lives, help their loved ones, or simply have fun. A <a target="_blank" class="text-blue-500 cursor-pointer"  href="https://www.undp.org/sites/g/files/zskgke326/files/2023-07/undp_informal_innovation_in_south_africa_final.pdf" > recent study </a> estimates one million grassroots innovators – 2.5% of the population – in South Africa alone. Scanning SDG Commons data can give a sense of what grassroots innovators are working on when it comes to inventing, developing, or adopting "green" technology that helps preserve the environment and a stable climate. We started by selecting the solutions mapped on the 58 African countries (about 3,000); next, we used a Large Language Model to select those that aligned with the definition of "green/enercgy/climate tech" used by the Timbuktoo initiative. Finally, we went through about 110 of the solutions most closely aligned with that definition, and created several boards, each one gathering solutions that deployed a specific technological strategy, like "develop affordable biodigesters to make use of the abundant organic waste and turn it into energy" or "connect machines to solar panels so that they can run even in remote locations where access to energy is limited". More on the methodology can be found <a target="_blank" class="text-blue-500 cursor-pointer" href=https://github.com/UNDP-Accelerator-Labs/timbuktoo-hub-solutions/blob/main/README.md">here</a>.' 
 			},
 				{
 					type: 'list',
 					items: [
-						'<b>Small-farm appropriate agricultural machines</b>. Most farms are small, but most machines for agriculture are conceived for large, industrial farms. A collection of simple, cheap machines that smallholder farmers can easily acquire and maintain. Solar pumps for irrigation in off-grid areas, pedal-powered cashew nuts peelers, olives presses, locally built and maintained electric mills...',
-						'<b>Advanced services to food systems</b>. Innovative services, often tailored for smallholder farmers. Market intelligence, logistics, e-commerce, and so on. Bringing them in line of sight of the farmers is about accelerating the adoption of innovation, rather than its generation.',
-						'<b>Smart and precision agriculture</b>. Solutions that bring a smart component to farming in Africa: irrigation based on soil moisture, observation and monitoring with computer vision tech, etc.',
-						'<b>Agro-ecology</b>. Solutions available to African farmers to grow crops and livestock in balance with nature. Insect protein as feedstock, paper production from grass instead of timber, organic methods to grow palms, etc.' ,
+						'<b>Green and clean energy</b>. These solutions answer to an unmet need for access to, saving of, and management of, energy in underserved communities. About half of them are aimed at production (often for immediate consumption by solar-powered machines– see below); the rest are mostly to save energy, with a few to transform it (for example alternators) or improve performance and safety.',
+						'<b>Solar-powered machines</b>. "Connecting X to a solar panel" is a technological trope in Africa, where many communities have limited access to robust electric grids. Water pumps, egg incubators, heaters, coolers, respirators... even hearing aids.',
+						'<b>Circular solutions</b>. Innovative services, often tailored for smallholder farmers. Market intelligence, logistics, e-commerce, and so on. Bringing them in line of sight of the farmers is about accelerating the adoption of innovation, rather than its generation.',
+						'<b>Biodigesters</b>. In the quest for access to energy, African innovators are experimenting with biodigesters of various types, almost always tailored to the need of the individual household, or of a few households at most. These solutions are appreciated as they transform an environmental bad (pollution from organic waste) into an economic good (energy).',
+						'<b>Clean and efficient cookers</b>. Firewood-powered cooking has long been known as a health hazard and a driver of deforestation. Many grassroots innovators in Africa are prototyping affordable, efficient, smokeless cookers.' ,
 					]
 				},
 			]
@@ -25,9 +26,11 @@ export const collection = {
 	],
 	highlights: [],
 	boards: [
-		510,
-		512,
-		514,
-		511,
+		519,
+		520,
+		523,
+		524,
+		531,
+		530
 	],
 }
