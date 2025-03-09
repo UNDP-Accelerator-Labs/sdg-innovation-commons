caa6538fbfb15a49fc54cbf840de54a1fc70853e
Alberto Cottica <alberto@cottica.net>
2025-03-09 11:07:20 +0100

    One-over to the greentech and agritech metaboards

--- app/lib/data/collection/tempData/african-solutions-greentech.ts
+++ app/lib/data/collection/tempData/african-solutions-greentech.ts
@@ -8,13 +8,17 @@ export const collection = {
 		{
 			items: [
 			{ 
+				type: 'txt', 
+				txt: 'Research shows that most innovation is not created in corporate- or university labs. Rather, it is created by ordinary people trying to improve their lives, help their loved ones, or simply have fun. A <a target="_blank" class="text-blue-500 cursor-pointer"  href="https://www.undp.org/sites/g/files/zskgke326/files/2023-07/undp_informal_innovation_in_south_africa_final.pdf"> recent study </a>, co-authored by researchers at MIT and the UNDP Accelerator Labs, estimates one million grassroots innovators – 2.5% of the population – in South Africa alone.'
+				},
 				{
 					type: 'txt', 
-					txt: 'Research shows that most innovation is not created in corporate- or university labs. Rather, it is created by ordinary people trying to improve their lives, help their loved ones, or simply have fun. A <a target="_blank" class="text-blue-500 cursor-pointer"  href="https://www.undp.org/sites/g/files/zskgke326/files/2023-07/undp_informal_innovation_in_south_africa_final.pdf"> recent study </a>, co-authored by researchers at MIT and the UNDP Accelerator Labs, estimates one million grassroots innovators – 2.5% of the population – in South Africa alone.'
-			},
-				type: 'txt', 
-				txt: 'Scanning SDG Commons data can give a sense of what grassroots innovators are working on when it comes to inventing, developing, or adopting "green" technology that helps preserve the environment and a stable climate. We started by selecting the solutions mapped on the 58 African countries (about 3,000); next, we used a Large Language Model to select those that aligned with the definition of "green/enercgy/climate tech" used by the Timbuktoo initiative. Finally, we went through about 110 of the solutions most closely aligned with that definition, and created several boards. Each board gathers solutions that deployed a specific technological strategy, for example "develop affordable biodigesters to make use of the abundant organic waste and turn it into energy" or "connect machines to solar panels so that they can run even in remote locations where access to energy is limited". More on the methodology can be found <a target="_blank" class="text-blue-500 cursor-pointer" href="https://github.com/UNDP-Accelerator-Labs/timbuktoo-hub-solutions/blob/main/README.md"">here</a>.' 
-			},
+					txt: 'Scanning SDG Commons data can give a sense of what grassroots innovators are working on when it comes to inventing, developing, or adopting "green" technology that helps preserve the environment and a stable climate. We started by selecting the solutions mapped on the 58 African countries (about 3,000); next, we used a Large Language Model to select those that aligned with the definition of "green/enercgy/climate tech" used by the Timbuktoo initiative. Finally, we went through 108 of the solutions most closely aligned with that definition, and created several boards. Each board gathers solutions that deployed a specific technological strategy, for example "develop affordable biodigesters to make use of the abundant organic waste and turn it into energy" or "connect machines to solar panels so that they can run even in remote locations where access to energy is limited". We next used various techniques to improve each board by removing false positives and finding  and adding false negatives. More on the methodology can be found <a target="_blank" class="text-blue-500 cursor-pointer" href="https://github.com/UNDP-Accelerator-Labs/timbuktoo-hub-solutions/wiki/Methods">here</a>.' 
+				},
+				{
+					type: 'txt', 
+					txt: 'This process left us with a collection of 383 solutions close to Timbuktoo\'s description of green, energy, and climate tech. Many of these cluster into the five boards described below. The five boards can be interpreted as a list of the green technologies that African grassroots innovators believe to be both in demand and achievable. The relative size of the boards has no particular meaning, as it is influenced by the interests of the Accelerator Labs in Africa.' 
+				},
 				{
 					type: 'list',
 					items: [
