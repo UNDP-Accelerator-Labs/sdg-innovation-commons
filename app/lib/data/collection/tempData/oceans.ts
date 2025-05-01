f01c15341c6e588ec7880cea7e3cba12c34d79d8
Adedapo Aderemi <adedapopaul@yahoo.com>
2025-05-01 13:05:19 +0100

    quick fix for pr conflict

---
+++ app/lib/data/collection/tempData/oceans.ts
@@ -0,0 +1,54 @@
+export const collection = {
+	id: 'oceans-conference',
+	title: 'Oceans-based sustainable development',
+	description: 'Through its R&D practice, UNDP is exploring new pathways to development that builds on the oceans\' bounty, while preserving their environmental balance and their beauty. Photo: Glenn Crouch on flickr.com.',
+	creatorName: 'Alberto Cottica',
+	mainImage: '/images/ocean_by_Glenn_Crouch.jpg',	
+	sections: [
+		{
+			items: [
+			{ 
+				type: 'txt', 
+				txt: 'This page contains an overview of the Research and Development (R&D) work themed on oceans-based sustainable development by the <a href="https://acceleratorlabs.undp.org">UNDP Accelerator Labs</a> in the period 2020-2025. It is means as a contribution to the preparation for the <a href="https://sdgs.un.org/conferences/ocean2025">Ocean Conference 2025</a>.  This work is highly decentralized, driven by national priorities; nevertheless, common patterns emerge. '
+				},
+				{
+					type: 'txt', 
+					txt: 'The Oceans Conference 2025 is organized into ten panels (<a href="https://sdgs.un.org/sites/default/files/2024-11/USG%20Letter%20to%20ECESA%20Plus_UNOC%202025_24-01789.pdf">source</a>). We ran a semantic search in the SDG Commons – powered by a Large Language Model trained on UNDP data to improve accuracy – using the descriptions of the panels as search terms, and manually assigned the best-fitting results to boards of the same name. Next, we looked for emerging patterns. We interpret the latter as opportunities: they reflect that R&D efforts all around the world, despite differences in context, singled out the same approaches as promising to advance the Sustainable Development Goals. We then collected the various instances of each pattern into a board. The raw data for this analysis include not only UNDP\' own R&D activities, but also many innovations introduced by local entrepreneurs and inventors in a bottom-up manner. The presence of many solutions around a pattern is a sign that local innovators, too, perceive that opportunity. We detected the following patterns:' 
+				},
+				{
+					type: 'list',
+					items: [
+					'<b>Aquaculture and fish farming</b>. Many coastal communities depend on catching and selling fish for their livelihood, but modern fishing can severely damage marine environments, leading to pollution, biodiversity reduction and ultimately environmental collapse. UNDP is experimenting with aquaculture and fish farming as a way to supply seafood while maintaining a much tighter control on the environmental consequences of doing so. Aquaculture can also be complementary to other economic activities, such as farming, and in this sense increase the diversity and resilience of coastal economies (Bangladesh, Lesotho – the latter for freshwater aquaculture). In freshwater-scarce environments, aquaculture can coexist with agriculture on the same land through polyculture (Barbados). Freshwater aquaculture can also lead to dilemmas about how to allocate water: UNDP is looking into methods to overcome them by using generative AI to simulate the impact of different solutions (Mexico).',
+					'<b>Seaweed and kelp</b> Seaweed and kelp are abundant in nature and rich in nutrients. They have the potential to produce human food, animal fodder or agricultural inputs such as fertilizers with much reduced environmental footprint than land agriculture, animal husbandry and fishing. UNDP teams are working across many countries (Bangladesh, Barbados, Mauritius, Namibia, Samoa, South Africa) to experiment with various species of seaweed, seagrass, kelp, seagrapes and so on. ',
+					'<b>Integration between fishing and tourism</b>. The rapid growth of tourism as a global industry is a mixed blessing for coastal communities. It benefits fisherfolk by creating additional, local demand for catch; on the down side, it puts additional pressure on marine environments and often leads to local frictions witin communities. For example, fisherfolk tend to view beaches (and the sea itself) as bases for their operations, where they leave their equipment and even abandon waste. Tourists, instead, see beaches as a tourist attraction, which should be well groomed. UNDP teams in several countries are working on reconciling the two. For example, Barbados connects fisherfolk with hotels, trying for shorter supply chains and a better livelihood for the fishing communities. Mauritius is experimenting on ways that artisanal fisherfolk, with their rich local knowledge can provide services to senior tourists. Cape Verde, Lesotho and Samoa pilot programmes where catch-and-release or otherwise highly sustainable fishing are offered to tourists as experiences. Trinidad and Tobago is experimenting with "artificial reefs" created by ',
+					'<b>Citizen science and participatory knowledge</b>. Effective action to conserve and regenerate marine environments needs up-to-date data and knowledge on their condition. Many local contexts suffer from data gaps. Additionally, that knowledge needs to be shared for communities to support restorative actions and participate in them. UNDP\'s response to this state of affairs is to engage citizens and communities in the production and socialization of the relevant data and knowledge. UNDP deployed citizen science initiatives to monitor marine ecosystems (Argentina) and marine litter (Panama); studied the seasonal cycles of activities alternative to fishing – and often performed by women – to better understand the implications of temporary fishing bans on coastal livelihoods (Bangladesh); equipped fishing vessels with networked sensors to study water quality (Cape Verde); ran ethnographies on waste and the circular economy to better understand the phenomenon of marine littering (Pacific, Pakistan, Philippines); designed and facilitated participatory dialogues to inform the government\'s position in negotiating a global agreement on plastic pollution (Mexico); organized solutions mapping campaigns (South Africa), innovation challenges (Pacific, Trinidad and Tobago) and "blue" hackathons (Cape Verde again) to benefit from the ingenuity of local innovators.',
+					'<b>Local engagement of coastal communities</b>. Development interventions can have no lasting impact without the informed consent, and ideally the active involvement, of the communities where those interventions take place. Yet, oftentimes actions to conserve marine environments (for example a seasonal fishing ban) or adopt more sustainable business models (For example, crafting tourism experiences from fishing expeditions) can feel abstract and distant. They tend to have long-term benefits far into the future, but demand investments of time and resources right here and now. UNDP teams across the planet deploy methods from collective intelligence and participatory design to build interventions that reconcile environmental protection with community prosperity. Powered by collective intelligence, these interventions tend to be highly creative. For example, UNDP Bangladesh has prototyped, and is now scaling, an initiative for island communities to become stewards of the sea turtles laying eggs on their beaches, and of their hatchlings. UNDP Indonesia worked with an interfaith alliance of religious-based charities to collect plastic waste, thereby preventing it from ending up in the water. UNDP Pacific deployed collective intelligence methods to co-design the services auxiliary to a project of solar electrification of some smaller islands (solar energy production fluctuates with sunlight, so such projects have the problem of what to do with the extra power generated on sunny days). UNDP Mauritius insisted on including fishing communities in the revamping of the islands\' tourist offer.',
+					'<b>Portfolios</b>. Marine and coastal ecosystems are inherently complex and resist single-point linear interventions. UNDP acknowledges that holistic portfolios of interventions are almost always necessary. Teams in Bangladesh, Barbados, Cape Verde and Guinea-Bissau are implementing Blue Economy portfolios, seeking to address marine conservation, biodiversity, and human prosperity goals through a coherent set of interventions.',
+					'<b>Crowdfunding</b>. UNDP Uruguay has experimented with crowdfunding as an innovative approach to financing marine ecosystems restoration.',
+					]
+				},
+				{
+					type: 'txt',
+					txt: 'The diagram below shows how these patterns support the panels in which the Oceans Conference is organized. The names of the panels have been shortened; their non-shortened list is <a href="https://sdgs.un.org/sites/default/files/2024-11/USG%20Letter%20to%20ECESA%20Plus_UNOC%202025_24-01789.pdf">here</a>. An interactive version of the diagram is visible <a href="https://public.flourish.studio/visualisation/22764683/">here</a>.'
+				},
+				{
+					type: 'img',
+					src: '/images/oceans-conference-sankey.png'
+				}
+			]
+		},
+	],
+	highlights: [],
+	boards: [
+		588,
+		589,
+		590,
+		592,
+		601,
+		604,
+		605,
+		606,
+		607,
+		608,
+	],
+}
