0c18d14d01422ae0e75a983a7cf5ce8976cc3455
Adedapo Aderemi <adedapopaul@yahoo.com>
2025-05-01 12:37:01 +0100

    quick fix

--- app/lib/data/collection/tempData/index.ts
+++ app/lib/data/collection/tempData/index.ts
@@ -5,7 +5,7 @@ import { collection as sustainableTourism } from './sustainable-tourism';
 import { collection as africanAgritech } from './african-solutions-agritech';
 import { collection as africanGreentech } from './african-solutions-greentech';
 import { collection as justice } from './justice';
-import { collection as oceans} from './oceans-conference';
+import { collection as oceans} from './oceans';
 
 export const collection = [
 	circularEcnomy,
@@ -15,5 +15,5 @@ export const collection = [
 	africanAgritech,
 	africanGreentech,
 	justice,
-	oceansConference,
+	oceans,
 ]
