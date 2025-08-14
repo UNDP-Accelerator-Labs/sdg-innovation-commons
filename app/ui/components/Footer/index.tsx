21d72ac31fdcfed6b9b603e32abf13c0fa233875
Adedapo Aderemi <adedapopaul@yahoo.com>
2025-08-14 04:56:48 +0100

    move API section into home

--- app/ui/components/Footer/index.tsx
+++ app/ui/components/Footer/index.tsx
@@ -14,36 +14,7 @@ export default function Footer() {
   const { isLogedIn, session } = sharedState || {}
   return (
     <>
-      <section id='api-documentation' className="home-section flex lg:mx-0 lg:py-[10px] grid-bg font-noto-sans">
-        <div className="inner mx-auto lg:w-[846px]">
-          <div className="box-border items-center justify-start gap-10 px-5 lg:px-0 py-[80px]">
-            <div className="relative self-stretch text-center">
-              <p className="m-0">
-                <b>
-                  <span className="text-sm leading-5 lg:text-[22px] lg:leading-[36px]">
-                    Build with SDG Commons Data
-                  </span>
-                </b>
-              </p>
-              <p className="my-5 pb-5 text-sm font-medium leading-5 lg:text-[20px] lg:leading-[28px]">
-                Access our databases of solutions, experiments, and insights through our developer-friendly APIs.
-                Integrate the collective intelligence of global sustainability efforts into your applications and research projects.
-              </p>
-
-              <Button>
-                <Link href="https://apis.sdg-innovation-commons.org/" target='_blank'>
-                  Explore API Documentation
-                </Link>
-                <svg className="w-5 h-5 transition-transform duration-300 group-hover:translate-x-1" viewBox="0 0 20 20" fill="currentColor">
-                  <path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-                </svg>
-              </Button>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section id='powered-by' className='home-section py-[40px]'>
+      <section className='home-section py-[40px]'>
         <div className='inner w-[375px] md:w-[744px] lg:w-[992px] xl:w-[1200px] xxl:w-[1440px] mx-auto px-[20px] lg:px-[80px] xl:px-[40px] xxl:px-[80px]'>
           <div className='section-content grid grid-cols-9 gap-[20px]'>
             <div className='col-span-9 lg:col-span-1'>
