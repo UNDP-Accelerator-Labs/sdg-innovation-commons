42ea9cc989e86ef1fb78fe00d36e81e1af367ad7
myjyby <myjyby@gmail.com>
2025-11-04 12:06:20 +0100

    added some TO DOs

--- __registries__/__init__.py
+++ __registries__/__init__.py
@@ -3,6 +3,10 @@ from .sdgs import tree as sdg_tree
 from .tags import tree as tag_tree
 from .years import tree as year_tree
 from .solutions import tree as solutions_tree
+"""
+TO DO: Import stories, elements, learningplans, etc.
+And add to registries list below.
+"""
 
 registries = [
   locations_tree,
