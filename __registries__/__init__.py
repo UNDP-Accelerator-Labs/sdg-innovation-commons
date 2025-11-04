71fd7f0f772c596cac4b0a072bee8122a3c4d860
myjyby <myjyby@gmail.com>
2025-11-04 11:25:34 +0100

    added types registry

--- __registries__/__init__.py
+++ __registries__/__init__.py
@@ -2,12 +2,14 @@ from .locations import tree as locations_tree
 from .sdgs import tree as sdg_tree
 from .tags import tree as tag_tree
 from .years import tree as year_tree
+from .solutions import tree as solutions_tree
 
 registries = [
   locations_tree,
   sdg_tree,
   tag_tree,
   year_tree,
+  solutions_tree,
 ]
 
 __all__ = [
